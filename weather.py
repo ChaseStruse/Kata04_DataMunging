5efd6d06d85f917b1456ed679b17a5fa878c2dda
Chase <chasestruse@gmail.com>
2020-07-04 18:35:39 -0500

    Retrieved max temps:

--- weather.py
+++ weather.py
@@ -9,4 +9,18 @@ weatherData = []
 for line in open(weatherFile):
     weatherData += [line.split()]
 
-print(weatherData)
+days = [weatherData[0] for x in weatherData]
+
+print(weatherData[2][1])
+
+def setMaxTemps(_weatherData):
+    _maxTemps = []
+    for day in weatherData:
+        _maxTemps += day[1]
+    return(_maxTemps)
+
+maxTemperatures = setMaxTemps(weatherData)
+
+def setLowTemps(_weatherData):
+    pass
+
